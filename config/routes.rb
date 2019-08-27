6ec27684935873cccfa327d6d1dd69dd072337ff
jesusmtz1991 <jesusmtz1991@gmail.com>
2019-08-27 00:15:03 -0700

    Setting the root page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'display' => 'home#Index'
   get 'create' => 'home#filldb'
   get 'delete' => 'home#destroy'
+  root 'home#Index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
